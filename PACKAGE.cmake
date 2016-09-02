a8e11264c0a2416ac7401d680024ac766f0f42b8
Erik van der Tier <mail@erikvandertier.com>
2016-09-02 21:50:36 +0200

    Added TRAILDB-CPP_LIB

--- PACKAGE.cmake
+++ PACKAGE.cmake
@@ -1,5 +1,6 @@
 include (${WORKSPACE}/traildb/PACKAGE.cmake)
 
 set (TRAILDB-CPP_INC ${INC_ROOT}/traildb-cpp/_/traildb-cpp)
+set (TRAILDB-CPP_LIB ${TRAILDB_LIB})
 
 include_directories(${TRAILDB-CPP_INC})
